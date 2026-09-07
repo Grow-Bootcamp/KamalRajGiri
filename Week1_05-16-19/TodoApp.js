83514abfe2f9bba191c42bb1b710298f340babf0
Kamal Raj Giri <girikamal2087@gmail.com>
2026-09-07 09:47:33 +0545

    Week 1 Work

---
+++ Week1_05-16-19/TodoApp.js
@@ -0,0 +1,9 @@
+console.log("Todo App Initiated...");
+
+(function update (){
+    const date = new Date();
+    document.getElementById('time').dateTime = date.toISOString();
+    document.getElementById('time').textContent = date.toLocaleString();
+    setTimeout(update,1000);
+})();
+
