219194ed5529dcc8f351ca7cac2a6d0e81910bbc
Kamal Raj Giri <girikamal2087@gmail.com>
2026-09-09 14:35:43 +0545

    Expense tracker

---
+++ Week2_05-22-26/W2D1_05_22/expense-tracker/models/Expense.js
@@ -0,0 +1,24 @@
+//demonstrating OOP using a class 
+export class Expense {
+
+    constructor(id, title, amount, category) {
+        this.id = id;
+        this.title = title;
+        this.amount = amount;
+        this.category = category;
+    }
+
+    getInfo() {
+        return `${this.title} - Rs. ${this.amount} - ${this.category}`;
+    }
+}
+
+// Example usage of the Expense class
+// const expense = new Expense(
+//     1,
+//     "Lunch",
+//     300,
+//     "Food"
+// );
+
+// console.log(expense.getInfo());
---
+++ Week2_05-22-26/W2D1_05_22/expense-tracker/utils/api.js
@@ -0,0 +1,30 @@
+const API_BASE_URL = "http://localhost:4000";
+
+export async function apiRequest(endpoint, options = {}) {
+  try {
+    const response = await fetch(`${API_BASE_URL}${endpoint}`, options);
+
+    let data = null;
+
+    const contentType = response.headers.get("content-type");
+
+    if (contentType && contentType.includes("application/json")) {
+      data = await response.json();
+    }
+
+    if (!response.ok) {
+      const error = new Error(
+        data?.message || `Request failed with status ${response.status}`,
+      );
+
+      error.status = response.status;
+      error.data = data;
+
+      throw error;
+    }
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
